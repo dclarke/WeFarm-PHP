a49ebe25f674e5afc0acb967e4bf5bc896afc0a9
Bryan Schwab <bschwab@gmail.com>
2013-09-16 13:16:53 -0700

    Formatting and links

--- application/views/user/account.php
+++ application/views/user/account.php
@@ -1,31 +1,37 @@
 <h1 class="floatLeft"><? echo $name; ?>'s Farm</h1>
-<p> <? echo $wepay ?></p>
-<p>
-  	<b>Name:</b>
-  	<? echo $name; ?>
-	</p>
 
+
+<? if (!$token) {
+}
+?>
+
+<p> <? echo $wepay; ?></p>
 <p>
-  	<b>Email:</b>
-  	<? echo $email; ?>
+<b>Name:</b>
+<? echo $name; ?>
+</p>
+<p>
+<b>Email:</b>
+<? echo $email; ?>
 </p>
 
 <p>
-	<b>Farm:</b>
-	<? echo $farm; ?>
+<b>Farm:</b>
+<? echo $farm; ?>
 </p>
 
 <p>
-  	<b>Produce:</b>
-  	<? echo $produce; ?>
+<b>Produce:</b>
+<? echo $produce; ?>
 </p>
 
 <p>
-  	<b>Produce price:</b>
-  	<? echo '$'.$price; ?>
+<b>Produce price:</b>
+<? echo '$'.$price; ?>
 </p>
-	<? if ($edit){
-		echo "<a href=".url::base().'/user/edit>Edit</a>';
-	}
-	?>
-	
+<p><p>
+<? if ($edit) {
+    echo "<a id=\"edit\" href=" . URL::base() . 'user/edit>Edit</a><p>';
+    echo "<a id=\"delete\" href=" . URL::base() . 'user/delete>Delete</a>';
+}
+?>
