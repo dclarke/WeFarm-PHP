4cc9d0961c1b4e17549389f93b97d4bcb020518f
Bryan Schwab <bschwab@gmail.com>
2013-09-16 11:48:09 -0700

    Rename and formatting

--- application/classes/Controller/User.php
+++ application/classes/Controller/User.php
@@ -2,18 +2,19 @@
 
 class Controller_User extends Controller_Base {
 
-	public function action_index()
-	{
+	public function action_index() {
 		if (Auth::instance()->logged_in()){
 			$user = Auth::instance()->get_user();
 			$farmer = ORM::factory('farmer')->where('email', '=', $user->email)->find();
 			$this->template->content = View::factory('user/account');
-			if (!($farmer->hasAccessToken())){
-				$this->template->content->wepay = "<b>Please create an account to manage your money: <a class='btn btn-primary' href=".url::base()."/wepayapi>Click here to create your account</a>";
-			}
-			else{
+			if (!($farmer->hasAccessToken())) {
+				$this->template->content->wepay = "<b>Please create an account to manage your money: <p><a class='wepay-widget-button wepay-blue' href=" . URL::base() . "wepayapi>Click here to create your WePay account</a>";
+				$this->template->content->token = false;
+			} else {
 				$this->template->content->wepay = '';
+				$this->template->content->token = true;
 			}
+
 			$this->template->content->name = $farmer->name;
 			$this->template->content->email = $farmer->email;
 			$this->template->content->farm = $farmer->farm;
@@ -21,41 +22,48 @@ class Controller_User extends Controller_Base {
 			$this->template->content->price = number_format($farmer->produce_price,2);
 			$this->template->content->edit = true;
 		}
-		else{
+		else {
 			$this->template->content = View::factory('welcome/index');
 		}
 	}
 
-	public function action_account(){
+	public function action_account() {
 		$id = Request::current()->param('id');
-		if (!isset($id)){
+		if (!isset($id)) {
 			HTTP::redirect('/');
 		}
 		$farmer = ORM::factory('farmer')->where('id', '=', $id)->find();
 		$this->template->content = View::factory('user/account');
-		if (Auth::instance()->logged_in()){
+		if (Auth::instance()->logged_in()) {
 			$user = Auth::instance()->get_user();
-			if ($farmer->email == $user->email){
+			if ($farmer->email == $user->email) {
 				$this->template->content->edit = true;
 			}
-			else{
+			else {
 				$this->template->content->edit = false;
 			}
-			if (!($farmer->hasAccessToken())){
-				$this->template->content->wepay = "<b>Please create an account to manage your money: <a class='btn btn-primary' href=".url::base()."/wepayapi>Click here to create your account</a>";
+
+			if ($farmer->hasAccessToken()) {
+				$this->template->content->token = true;
 			}
-			else if (!($this->template->content->edit) && $farmer->hasAccountId()){
-				$this->template->content->wepay = "<a href=".url::base()."/user/buy/".$id." class='btn btn-danger btn-large' id='buy-now-button'>Buy ".$farmer->produce." Now!</a>";
+
+			if (!($farmer->hasAccessToken())) {
+				$this->template->content->wepay = "<b>Please create an account to manage your money: <p><a class='wepay-widget-button wepay-blue' href=" . URL::base() . "wepayapi>Click here to create your WePay account</a>";
+				$this->template->content->token = false;
 			}
-			else{
+			else if (!($this->template->content->edit) && $farmer->hasAccountId()) {
+				$this->template->content->wepay = "<a href=" . URL::base() . "/user/buy/".$id." class='btn btn-danger btn-large' id='buy-now-button'>Buy ".$farmer->produce." Now!</a>";
+			}
+			else {
 				$this->template->content->wepay = '';
 			}
 		}
-		else{
+		else {
 			$this->template->content->wepay = '';
-			if ($farmer->hasAccountId()){
-				$this->template->content->wepay = "<a href=".url::base()."/user/buy/".$id." class='btn btn-danger btn-large' id='buy-now-button'>Buy ".$farmer->produce." Now!</a>";
+			if ($farmer->hasAccountId()) {
+				$this->template->content->wepay = "<a href=". URL::base() . "/user/buy/".$id." class='btn btn-danger btn-large' id='buy-now-button'>Buy ".$farmer->produce." Now!</a>";
 			}
+			$this->template->content->token = false;
 			$this->template->content->edit = false;
 		}
 		$this->template->content->name = $farmer->name;
@@ -65,16 +73,23 @@ class Controller_User extends Controller_Base {
 		$this->template->content->price = number_format($farmer->produce_price,2);
 	}
 
-	public function action_buy(){
+	public function action_buy() {
 		$id = Request::current()->param('id');
-		if (!isset($id)){
+		if (!isset($id)) {
 			HTTP::redirect('/');
 		}
-		$farmer = ORM::factory('farmer')->where('id', '=', $id)->find();
-		$checkout_uri = Controller_WepayAPI::create_checkout($farmer);
-		$this->template->content = View::factory('user/buy');
+        $farmer = ORM::factory('farmer')->where('id', '=', $id)->find();
+
+        try {
+            $checkout_uri = Controller_Wepayapi::create_checkout($farmer);
+        } catch (WePayPermissionException $e) {
+            $this->template->content = "There was an error: " . $e->getMessage();
+            return;
+        }
+
+        $this->template->content = View::factory('user/buy');
 		$this->template->content->checkout_uri = $checkout_uri;
-		$this->template->content->return_uri = url::base().'/user/account/'.$id;
+		$this->template->content->return_uri = URL::base() . '/user/account/'.$id;
 		$this->template->content->name = $farmer->name;
 		$this->template->content->email = $farmer->email;
 		$this->template->content->farm = $farmer->farm;
@@ -86,46 +101,62 @@ class Controller_User extends Controller_Base {
 		$this->template->content = View::factory('user/register');
 	}
 
-	public function action_complete_registration(){
-		try{
-			$post = Validation::factory($_POST)
-            ->rule('name', 'not_empty')
+	public function action_complete_registration() {
+
+        $validation = Validation::factory($this->request->post())
+            ->rule('username', 'not_empty')
             ->rule('password', 'not_empty')
-            ->rule('password', 'min_length', array('6'))
+            ->rule('password', 'min_length', array(':value', 6))
             ->rule('email', 'not_empty')
             ->rule('email', 'email')
             ->rule('price', 'numeric')
             ->rule('price', 'not_empty')
             ->rule('farm', 'not_empty')
             ->rule('produce', 'not_empty');
-		} catch (Validation_Exception $e) {
-			$this->template->content = "Your registration was not valid: ".$e->errors();
-		}
-		
-		$user = ORM::factory('user');
-		$farmer = ORM::factory('farmer');
 
-		$user->username = $_POST['name'];
+        // Validation check
+        if (!$validation->check()) {
+            $errors = $validation->errors('user');
+			$this->template->content = "Your registration was not valid!";
+            return;
+        }
+
+        // Create User
+        $user = ORM::factory('User');
+		$user->username = $_POST['username'];
 		$user->email = $_POST['email'];
 		$user->password = $_POST['password'];
-		
-		$farmer->name = $_POST['name'];
+
+        try {
+            $user->save();
+        } catch (ORM_Validation_Exception $e) {
+            $this->template->content = "There was a problem creating your user: " . var_dump($e->errors());
+            return;
+        }
+
+        // Create Farmer
+		$farmer = ORM::factory('farmer');
+		$farmer->name = $_POST['username'];
 		$farmer->email = $_POST['email'];
 		$farmer->farm = $_POST['farm'];
 		$farmer->produce = $_POST['produce'];
 		$farmer->produce_price = $_POST['price'];
 
-		$user->save();
-		$user->add('roles', ORM::factory('role')->where('name', '=', 'login')->find());
-		$farmer->save();
+        // Add login role
+        $user->add('roles', ORM::factory('Role', array('name' => 'login')));
+
+        try {
+            $farmer->save();
+        } catch (ORM_Validation_Exception $e) {
+            $this->template->content = "There was a problem creating your farmer: " . var_dump($e->errors());
+        }
 
 		$success = Auth::instance()->login($_POST['email'], $_POST['password']);
 
-		if ($success){
+		if ($success) {
 			HTTP::redirect('user');
-		}
-		else{
-			$this->template->content = "There was an error";
+		} else{
+			$this->template->content = "There was an error!";
 		}
 	}
 
@@ -177,8 +208,24 @@ class Controller_User extends Controller_Base {
 		else{
 			$this->template->content = "Error, you're not logged in!";
 		}
-
 	}
+
+    public function action_delete() {
+		if (Auth::instance()->logged_in()){
+			$this->template->content = "Delete? Really?";
+			$user = Auth::instance()->get_user();
+			$farmer = ORM::factory('farmer')->where('email', '=', $user->email)->find();
+
+		    Auth::instance()->logout();
+            $farmer->delete();
+            $user->delete();
+		    HTTP::redirect('/');	
+		}
+		else{
+			$this->template->content = "Error, you're not logged in!";
+		}
+
+    }
 
 	public function action_update(){
 		try{
@@ -207,6 +254,5 @@ class Controller_User extends Controller_Base {
 		else{
 			$this->template->content = "You can't update information for this user!";
 		}	
-
 	}
 } 
