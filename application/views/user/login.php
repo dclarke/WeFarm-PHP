0c10c161c65ec124a493e832bd984cc767b9aaf3
Bryan Schwab <bschwab@gmail.com>
2013-09-16 13:13:23 -0700

    Fixing form

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -1,17 +1,17 @@
 <h1>Farmer Login</h1>
 <div class="row-fluid">
 	<div class="span8">	
-		<?php echo form::open('user/complete_login') ?>
+		<?php echo Form::open('user/complete_login') ?>
 			<div class="field">
     			<label for="farmer_email">Email</label>
-			    <?php echo form::input('email','',array('id' => 'email')) ?>
+			    <?php echo Form::input('email','',array('id' => 'email')) ?>
 			</div>
 			<div class="field">
 				<label for="farmer_password">Password</label>
-				<?php echo form::password('password','',array('id' => 'password')) ?>
+				<?php echo Form::password('password','',array('id' => 'password')) ?>
 			</div>
 			<div class="actions">
-   	 			<?php echo form::submit('submit','Login') ?>
+   	 			<?php echo Form::submit('submit','Login') ?>
   			</div>
 	</div>
 </div>
