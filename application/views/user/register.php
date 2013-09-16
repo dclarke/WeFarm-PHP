91b1f20d7ab8db45c6a180b590bc4a7b7465a964
Bryan Schwab <bschwab@gmail.com>
2013-09-16 13:12:59 -0700

    Fixing form

--- application/views/user/register.php
+++ application/views/user/register.php
@@ -1,33 +1,33 @@
 <h1>New farmer registration</h1>
-<?php echo form::open('user/complete_registration') ?>
+<?php echo Form::open('user/complete_registration') ?>
 
   <div class="field">
     <label for="farmer_name">Name</label>
-    <?php echo form::input('name','',array('id' => 'name')) ?>
+    <?php echo Form::input('username','',array('id' => 'username')) ?>
   </div>
   <div class="field">
     <label for="farmer_email">Email</label>
-    <?php echo form::input('email','',array('id' => 'email')) ?>
+    <?php echo Form::input('email','',array('id' => 'email')) ?>
   </div>
   <div class="field">
     <label for="farmer_password">Password</label>
-    <?php echo form::password('password','',array('id' => 'password')) ?>
+    <?php echo Form::password('password','',array('id' => 'password')) ?>
   </div>
   <div class="field">
     <label for="farmer_farm">Farm</label>
-    <?php echo form::input('farm','',array('id' => 'farm')) ?>
+    <?php echo Form::input('farm','',array('id' => 'farm')) ?>
   </div>
   <div class="field">
     <label for="farmer_produce">Produce</label>
-    <?php echo form::input('produce','',array('id' => 'produce')) ?>
+    <?php echo Form::input('produce','',array('id' => 'produce')) ?>
   </div>
   <div class="field">
     <label for="farmer_produce_price">Produce price</label>
-    <?php echo form::input('price','',array('id' => 'price')) ?>
+    <?php echo Form::input('price','',array('id' => 'price')) ?>
   </div>
   <div class="actions">
-    <?php echo form::submit('submit','Create Farmer') ?>
+    <?php echo Form::submit('submit','Create Farmer') ?>
   </div>
-</form>
+</Form>
 
 <a href="/wefarm_php/">Back</a>
