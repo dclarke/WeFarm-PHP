3194a373def3877f950f2760199838f42f5b4499
Bryan Schwab <bschwab@gmail.com>
2013-09-23 16:54:10 -0700

    API config file

---
+++ application/config/wepay.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array(
+
+        // Your Client ID
+	'client_id'       => '153336',
+
+        // Your Client Secret
+	'client_secret'   => 'e913db7934',
+);
